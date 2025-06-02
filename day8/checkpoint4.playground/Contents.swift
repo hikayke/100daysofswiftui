474c7bfcf21c90101adf9021d9bf96135f755a4b
hikayke <45440657+hikayke@users.noreply.github.com>
2025-06-02 18:21:26 +0100

    Updated checkpoint 4 - Added out of bounds check

--- day8/checkpoint4.playground/Contents.swift
+++ day8/checkpoint4.playground/Contents.swift
@@ -1,5 +1,20 @@
 import Cocoa
 
-enum input: Error {
+enum inputError: Error {
     case noSqrt, outOfBounds
 }
+
+func squareRoot(inputNumber: Int) throws {
+    var answer = 1
+    
+    if inputNumber < 1 || inputNumber > 10_000 {
+        throw inputError.outOfBounds
+    }
+}
+
+do {
+    try squareRoot(inputNumber: 20)
+} catch inputError.outOfBounds {
+    print("The number is out of bounds.")
+}
+
