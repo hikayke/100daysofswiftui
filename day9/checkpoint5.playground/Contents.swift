45846462e6bd0dffdda3228d2aa16b6c2a4097c7
hikayke <45440657+hikayke@users.noreply.github.com>
2025-06-18 16:33:44 +0100

    Added checkpoint 5

--- day9/checkpoint5.playground/Contents.swift
+++ day9/checkpoint5.playground/Contents.swift
@@ -1,1 +1,13 @@
 import Cocoa
+
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+let filterEven = luckyNumbers.filter { $0 % 2 == 0 }
+
+let sortedNumbers = luckyNumbers.sorted()
+
+let mappedNumbers = filterEven.map {String($0)}
+
+print(filterEven)
+print(sortedNumbers)
+print(mappedNumbers)
