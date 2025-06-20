c5fac1c030fc20514ab3801fe94107c17d649103
hikayke <45440657+hikayke@users.noreply.github.com>
2025-06-20 01:19:06 +0100

    Removed temporary variable - asked for help with warning

--- day9/checkpoint5.playground/Contents.swift
+++ day9/checkpoint5.playground/Contents.swift
@@ -15,8 +15,4 @@ print(mappedNumbers) */
 
 let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
 
-let closureNumbers = luckyNumbers.filter({$0 % 2 != 0}).sorted().map({String($0)})
-
-for number in closureNumbers {
-    print("\(number) is a lucky number.")
-}
+let closureNumbers = luckyNumbers.filter({$0 % 2 != 0}).sorted().map({print("\($0) is a lucky number.")})
