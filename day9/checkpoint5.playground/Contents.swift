870bea00f2bdb162691c92e87ea53bbf63243973
hikayke <45440657+hikayke@users.noreply.github.com>
2025-06-23 01:21:33 +0100

    Added checkpoint 6

--- day9/checkpoint5.playground/Contents.swift
+++ day9/checkpoint5.playground/Contents.swift
@@ -15,6 +15,12 @@ print(mappedNumbers) */
 
 let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
 
-let closureNumbers = luckyNumbers.filter({$0 % 2 != 0}).sorted().map({print("\($0) is a lucky number.")})
+// let closureNumbers = luckyNumbers.filter({$0 % 2 != 0}).sorted().map({print("\($0) is a lucky number.")})
 
-// getting "Constant 'closureNumbers' inferred to have type '[()]', which may be unexpected" warning (warning, not error). It works. I'll try to polish the code to get rid of the warning, I'm pretty sure it's because it's very cluttered.
+let closureNumbers = luckyNumbers
+    .filter { $0 % 2 != 0 }
+    .sorted ()
+    .map { print("\($0) is a lucky number.") }
+
+
+print(closureNumbers)
