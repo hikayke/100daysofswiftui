3d13b3a35dd9792f033eec6b98725941be5b7242
hikayke <45440657+hikayke@users.noreply.github.com>
2025-06-26 01:18:41 +0100

    Finished checkpoint 6

--- day11/checkpoint6.playground/Contents.swift
+++ day11/checkpoint6.playground/Contents.swift
@@ -3,26 +3,41 @@ import Cocoa
 // create a struct to store information about a car, including its model, number of seats, and current gear, then add a method to change gears up or down
 
 
-struct Car {
+struct myCar {
     public let model = "Beetle"
     public let seats = 4
     private(set) var currentGear: Int
     
-    init(currentGear: Int = 0) {
+    init(currentGear: Int = 1) {
         self.currentGear = currentGear
     }
-    
+
     mutating func gearUp() {
         currentGear += 1
+        validadeGear()
     }
     
     mutating func gearDown() {
         currentGear -= 1
+        validadeGear()
+    }
+    
+    func validadeGear() -> Bool {
+        if currentGear > 10 || currentGear < 1 {
+            print("Invalid gear.")
+            return false
+        } else {
+            print("Your gear is \(currentGear)")
+            return true
+        }
     }
     
 }
 
-var race1 = Car(currentGear: 2)
-Car.gearUp()
+var race1 = myCar(currentGear: 10)
+race1.gearDown()
+race1.gearUp()
+race1.gearUp()
+
 
 
