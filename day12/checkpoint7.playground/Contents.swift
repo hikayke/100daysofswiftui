bb9e4a7f5a3e1dede734b7554e95be1aaf71b840
hikayke <45440657+hikayke@users.noreply.github.com>
2025-06-26 21:27:11 +0100

    Added checkpoint 7

---
+++ day12/checkpoint7.playground/Contents.swift
@@ -0,0 +1,33 @@
+class Animals {
+    
+    var legs: Int
+    
+    init(legs: Int) {
+        self.legs = legs
+    }
+}
+
+
+class Dog: Animals {
+    
+}
+
+class Cat: Animals {
+    
+}
+
+class Corgi: Dog {
+    
+}
+
+class Poodle: Dog {
+    
+}
+
+class Persian: Cat {
+    
+}
+
+class Lion: Cat {
+    
+}
